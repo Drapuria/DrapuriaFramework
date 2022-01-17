6bc94be8aafc17c746fff0571bc765ece94def59
Marinus Vantrex <vantrexplays@gmail.com>
2022-01-17 20:26:18 +0100

    Added documentation

--- core/core-scheduler/src/main/java/net/drapuria/framework/scheduler/factory/SchedulerFactory.java
+++ core/core-scheduler/src/main/java/net/drapuria/framework/scheduler/factory/SchedulerFactory.java
@@ -21,6 +21,12 @@ import java.util.function.Supplier;
 
 import static lombok.AccessLevel.NONE;
 
+/**
+ * Uses chained setters to create {@link Scheduler<T>}
+ *
+ *
+ * @param <T> The Type that is used in the {@link Supplier}
+ */
 @Accessors(fluent = true, chain = true)
 @Setter
 public class SchedulerFactory<T> {
@@ -31,47 +37,121 @@ public class SchedulerFactory<T> {
     private final Map<Long, Consumer<T>> scheduledEvents = new HashMap<>();
     @Setter(NONE)
     private final Map<Timestamp, Consumer<T>> timedEvents = new HashMap<>();
+
+    /**
+     * The initial delay of the scheduler in {@link TickTime} (1 Tick equals 50ms)
+     */
     private long delay;
+
+
+    /**
+     * The period of the scheduler in {@link TickTime} (1 Tick equals 50ms)
+     */
     private long period;
+
+    /**
+     * The amount of iterations the {@link Scheduler<T>} should run.
+     *
+     *
+     * If set to {@code -1} the {@link Scheduler<T>} will run forever.
+     */
     private long iterations = -2;
+
+    /**
+     * The object supplied to the {@link Scheduler<T>}
+     */
     private Supplier<T> supplier;
+
+    /**
+     * The {@link AbstractSchedulerProvider} of the {@link Scheduler<T>}
+     */
     private Class<? extends AbstractSchedulerProvider> provider = ThreadedSchedulerProvider.class;
 
+    /**
+     * Sets the wrapped delay of the scheduler while converting it to ticks
+     *
+     * @param delay the delay in units
+     * @param tickTime The TickTime
+     * @return {@link SchedulerFactory<T>}
+     */
     public SchedulerFactory<T> delay(long delay, TickTime tickTime) {
         return delay(tickTime.getTicks() * delay);
     }
 
+
+    /**
+     * Sets the wrapped delay of the scheduler while converting it to ticks
+     *
+     * @param period the delay in units
+     * @param tickTime The TickTime
+     * @return {@link SchedulerFactory<T>}
+     */
     public SchedulerFactory<T> period(long period, TickTime tickTime) {
         return period(tickTime.getTicks() * period);
     }
 
+
+    /**
+     * Adds a {@link RepeatedAction<T>} for {@link Scheduler<T>}
+     *
+     * @param repeatedAction repeatedly performed action
+     * @return {@link SchedulerFactory<T>}
+     */
     public SchedulerFactory<T> repeated(RepeatedAction<T> repeatedAction) {
         this.repeatedActions.add(repeatedAction);
         return this;
     }
 
+    /**
+     * Adds a always performing {@link RepeatedAction<T>} for {@link Scheduler<T>}
+     *
+     * @param repeatedAction repeatedly performed action
+     * @return {@link SchedulerFactory<T>}
+     */
     public SchedulerFactory<T> repeated(BiConsumer<Long, T> repeatedAction) {
         return repeated(new RepeatedAction<>(true, true, true, 0, 0, repeatedAction));
     }
 
-    public SchedulerFactory<T> at(Timestamp timestamp, Consumer<T> event) {
+    /**
+     * Adds a {@link ScheduledAction<T>} for {@link Scheduler<T>}
+     *
+     * @param timestamp The time at which the action should be executed
+     * @param action The performed action
+     * @return {@link SchedulerFactory<T>}
+     */
+    public SchedulerFactory<T> at(Timestamp timestamp, Consumer<T> action) {
         if (this.iterations == -2) {
-            this.timedEvents.put(timestamp, event);
+            this.timedEvents.put(timestamp, action);
             return this;
-        } else return at(SchedulerHelper.convertTimestampToIteration(timestamp, iterations), event);
+        } else return at(SchedulerHelper.convertTimestampToIteration(timestamp, iterations), action);
     }
 
-    public SchedulerFactory<T> at(long iteration, Consumer<T> event) {
-        this.scheduledEvents.put(iteration, event);
+    /**
+     * Adds a {@link ScheduledAction<T>} for {@link Scheduler<T>}
+     *
+     * @param iteration The time at which the action should be exected
+     * @param action The performed action
+     * @return {@link SchedulerFactory<T>}
+     */
+    public SchedulerFactory<T> at(long iteration, Consumer<T> action) {
+        this.scheduledEvents.put(iteration, action);
         return this;
     }
 
+    /**
+     * Converts the {@link Timestamp} actions to a {@link Long}
+     */
     private void buildInternal() {
         this.timedEvents.forEach((timestamp, consumer) -> {
             this.scheduledEvents.put(SchedulerHelper.convertTimestampToIteration(timestamp, this.iterations), consumer);
         });
     }
 
+    /**
+     * Builds the {@link Scheduler<T>}
+     *
+     * @return {@link Scheduler<T>}
+     */
     public Scheduler<?> build() {
         buildInternal();
         Scheduler<T> scheduler = new Scheduler<>(delay, period, iterations);

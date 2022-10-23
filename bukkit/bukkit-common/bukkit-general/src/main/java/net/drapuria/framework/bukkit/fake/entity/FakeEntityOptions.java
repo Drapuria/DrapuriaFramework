a98349e9e96b45a126613d00725e4710e624d7d5
Vantrex <vantrexplays@gmail.com>
2022-10-23 15:09:11 +0200

    Added Builder to FakeEntityOptions

--- bukkit/bukkit-common/bukkit-general/src/main/java/net/drapuria/framework/bukkit/fake/entity/FakeEntityOptions.java
+++ bukkit/bukkit-common/bukkit-general/src/main/java/net/drapuria/framework/bukkit/fake/entity/FakeEntityOptions.java
@@ -1,6 +1,7 @@
 package net.drapuria.framework.bukkit.fake.entity;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import net.drapuria.framework.bukkit.fake.FakeShowType;
@@ -10,6 +11,7 @@ import java.util.Set;
 
 @Getter
 @Setter
+@Builder
 public class FakeEntityOptions {
 
     private String name;

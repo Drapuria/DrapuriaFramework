2da3d777cf0151aa0681b6be3528a9912743b92a
Marinus <vantrexplays@gmail.com>
2023-04-05 21:21:22 +0200

    Added Configuration beans

---
+++ core/core-beans/src/main/java/net/drapuria/framework/beans/configuration/AbstractConfigurationProvider.java
@@ -0,0 +1,25 @@
+package net.drapuria.framework.beans.configuration;
+
+
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Provides all the necessary methods for a Configurations Class which implements a Configuration
+ *
+ * @param <T> The Interface this provider handles
+ */
+public abstract class AbstractConfigurationProvider<T> {
+
+
+    public abstract void onEnable(T config);
+
+    public abstract void onPostEnable(T config);
+
+    public void internalOnEnable(@Nullable Object instance) {
+        onEnable((T) instance);
+    }
+
+    public void internalOnPostEnable(@Nullable Object instance) {
+        onPostEnable((T) instance);
+    }
+}
